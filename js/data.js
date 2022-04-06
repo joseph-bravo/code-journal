da3affef2c57de11c5c593eaa928ae082e15919a
Joseph Bravo <josephangelobravo@gmail.com>
2022-04-06 22:54:14 +0000

    added a few methods to data

--- js/data.js
+++ js/data.js
@@ -12,7 +12,13 @@ var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
-  nextEntryId: 1
+  nextEntryId: 1,
+  getEntryObject: function (id) {
+    return this.entries[id];
+  },
+  getEntryDom: function (id) {
+    return this.entries[id].dom;
+  }
 };
 
 function updateStoredData() {
@@ -20,9 +26,11 @@ function updateStoredData() {
 }
 
 function readStoredData() {
-  var storedData = localStorage.getItem('data');
+  var storedData = JSON.parse(localStorage.getItem('data'));
   if (storedData) {
-    data = JSON.parse(storedData);
+    for (var prop in storedData) {
+      data[prop] = storedData[prop];
+    }
     for (var i = 0; i < data.entries.length; i++) {
       data.entries[i].dateCreated = new Date(data.entries[i].dateCreated);
       data.entries[i].lastModified = new Date(data.entries[i].lastModified);
